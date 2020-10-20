047d520f371e585295241b3e5978005667794264
iampangpang123 <iamispangpang@163.com>
2020-10-20 13:52:09 +0800

    1

--- src/main/java/com/springboottemplate/task/Task1.java
+++ src/main/java/com/springboottemplate/task/Task1.java
@@ -1,18 +1,23 @@
 package com.springboottemplate.task;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
 
 /**
  * @Description:定时任务1
  * @author: 唐涛
  * @date: 2020年3月25日 下午2:05:20
- * 
+ *
  */
 
 /*
  * @Component 必须加
- * 
+ *
  */
 @Component
 public class Task1 {
@@ -22,15 +27,39 @@ public class Task1 {
 
 	/*
 	 * @Scheduled(fixedRate = 5000) ：上一次开始执行时间点之后5秒再执行
-	 * 
+	 *
 	 * @Scheduled(fixedDelay = 5000) ：上一次执行完毕时间点之后5秒再执行
-	 * 
+	 *
 	 * @Scheduled(initialDelay=1000, fixedRate=5000)
 	 * ：第一次延迟1秒后执行，之后按fixedRate的规则每5秒执行一次
 	 */
-	@Scheduled(fixedRate = 10000)
-	public void printTime() {
-		log.info("日志级别info" + "定时任务，现在时间：" + System.currentTimeMillis());
-		log.error("日志级别error" + "定时任务，现在时间：" + System.currentTimeMillis());
+//	@Scheduled(fixedRate = 10000)
+//	public void printTime() {
+//		log.info("日志级别info" + "定时任务，现在时间：" + System.currentTimeMillis());
+//		log.error("日志级别error" + "定时任务，现在时间：" + System.currentTimeMillis());
+//
+//	}
+
+	/**
+	 **
+	 * @Description:测试版本号，pom的java版本是1.6
+	 * @param:
+	 * @return: void
+	 */
+	@Test
+	public void test2() {
+		List  items=new ArrayList<String>();
+	      // Lambda 表达式遍历（JDK 1.8）
+        System.out.println("\n第三种遍历方式：Lambda 表达式遍历 List 集合");
+        items.forEach(item->{
+            System.out.println(item);
+        });
+	}
+
+	@Test
+	public void test3() {
+
+
+		log.error("你好：{}，李四，王五","张三");
 	}
 }
---
+++ src/main/java/com/springboottemplate/task/ThreadTask.java
@@ -0,0 +1,51 @@
+package com.springboottemplate.task;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @description:
+ * @author: ttao
+ * @create: 2020-09-27 19:14
+ **/
+@Component
+public class ThreadTask {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ThreadTask.class);
+    /*
+     * @Scheduled(fixedRate = 5000) ：上一次开始执行时间点之后5秒再执行
+     *
+     * @Scheduled(fixedDelay = 5000) ：上一次执行完毕时间点之后5秒再执行
+     *
+     * @Scheduled(initialDelay=1000, fixedRate=5000)
+     * ：第一次延迟1秒后执行，之后按fixedRate的规则每5秒执行一次
+     */
+
+    ExecutorService fixedThreadPool= Executors.newFixedThreadPool(8);
+    @Scheduled(cron =" 0/5 * * * * ?" )
+    public void printTime() {
+        for (int i=1;i<=2;i++){
+            fixedThreadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    //业务处理
+                    dosomeing();
+                }
+            });
+        }
+
+    }
+    //业务处理
+    @Async
+    void dosomeing(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String str = sdf.format(new Date());
+        System.out.println("线程名称："+Thread.currentThread().getName()+"时间:"+str);
+    }
+
+}
