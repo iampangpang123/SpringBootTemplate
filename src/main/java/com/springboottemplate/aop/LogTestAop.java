e6f172eb90fa5b753cc7a4acd05943cc64793588
iampangpang123 <iamispangpang@163.com>
2020-12-22 23:44:28 +0800

    自定义注解增加注释

--- src/main/java/com/springboottemplate/aop/LogTestAop.java
+++ src/main/java/com/springboottemplate/aop/LogTestAop.java
@@ -1,6 +1,5 @@
 package com.springboottemplate.aop;
 
-import com.alibaba.fastjson.JSON;
 import com.springboottemplate.annontation.LogTestAnno;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -31,7 +30,13 @@ public class LogTestAop {
 
     }
 
-
+    /**
+     * Signature getSignature();	获取封装了署名信息的对象,在该对象中可以获取到目标方法名,所属类的Class等信息
+     * Object[] getArgs();	获取传入目标方法的参数对象
+     * Object getTarget();	获取被代理的对象
+     * Object getThis();	获取代理对象
+     * @param joinPoint
+     */
     //配置切面
     @Before("poincut()")
     public void  advice(JoinPoint joinPoint){
@@ -43,7 +48,6 @@ public class LogTestAop {
 
             if (method.isAnnotationPresent(LogTestAnno.class)){
                 LogTestAnno annotation = method.getAnnotation(LogTestAnno.class);
-
                 logger.info("注解LogTestAnno生效了，生效类名："+aClass .getSimpleName()+"  方法名："+method.getName()+"  注解得值:"+annotation.name());
             }
 
